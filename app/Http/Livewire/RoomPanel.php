37310a028ad3860ea1671c1ede9b25819482b790
Brad Root <bradroot@me.com>
2021-12-05 21:43:44 -0800

    generate stairwell before all rooms have been explored

--- app/Http/Livewire/RoomPanel.php
+++ app/Http/Livewire/RoomPanel.php
@@ -24,21 +24,27 @@ class RoomPanel extends Component
         $next_room = Room::findOrFail($id);
         if (!$next_room->visited) {
             // Are there any other unvisited rooms?
-            $unvisited_room_count = Room::where('z', $next_room->z)->where('character_id', $character->id)->where('visited', False)->count();
-            if ($unvisited_room_count == 1) {
-                $floor = new Floor();
-                $floor->character_id = $character->id;
-                $floor->user_id = $character->user_id;
-                $floor->z = $next_room->z+1;
-                $floor->save();
+            $unvisited_rooms = Room::where('character_id', $character->id)->where('z', $next_room->z)->where('visited', False)->get();
+            if (count($unvisited_rooms) <= 4) {
+                $floor = Floor::where('character_id', $character->id)->where('z', $next_room->z+1)->first();
+                if (is_null($floor)) {
+                    $stair_room = $unvisited_rooms->random();
+                    $floor = new Floor();
+                    $floor->character_id = $character->id;
+                    $floor->user_id = $character->user_id;
+                    $floor->z = $stair_room->z+1;
+                    $floor->save();
 
-                $floor->generate($next_room->x,$next_room->y,$floor->z);
-                $next_room->below = $floor->first_room->id;
-                $floor->first_room->above = $next_room->id;
-                $floor->first_room->save();
+                    $floor->generate($stair_room->x,$stair_room->y,$floor->z);
+                    $stair_room->below = $floor->first_room->id;
+                    $floor->first_room->above = $stair_room->id;
+                    $floor->first_room->save();
+                    $stair_room->save();
+                }
             }
             $next_room->visited = True;
             $next_room->save();
+            $next_room->refresh();
         }
 
         $this->room = $next_room;
